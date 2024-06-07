630baf1199790e09027cb7a38103c21a3f73409a
kholodovaMB <holodova.maryna@gmail.com>
2024-06-07 20:44:30 +0300

    test Dockerfile

--- src/main/java/org/HMB/Main.java
+++ src/main/java/org/HMB/Main.java
@@ -3,6 +3,6 @@ package org.HMB;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Actions tests CI");
+        System.out.println("Hello Actions tests CI and docker");
     }
 }
